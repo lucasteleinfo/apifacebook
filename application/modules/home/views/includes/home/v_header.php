5bf3752989ccea67fc2593e01fdf4a8c2a44876c
Lucas França <lucas.teleinfo@gmail.com>
2015-12-07 06:04:21 -0300

    Incluindo meta tags do Facebook

--- application/modules/home/views/includes/home/v_header.php
+++ application/modules/home/views/includes/home/v_header.php
@@ -5,6 +5,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
 <meta charset="UTF-8">
+
+<meta property="og:locale" content="pt_BR">
+<!-- Link da publicação -->
+<meta property="og:url" content="http://www.lfranca.com.br/facebook">
+<!-- Link da Titulo da Publicação -->
+<meta property="og:title" content="Utilizando as meta tags do Facebook.">
+<!-- Titulo do site -->
+<meta property="og:site_name" content="Teste APIs Facebook">
+<!-- Descrição prévia da publicação -->
+<meta property="og:description" content="O tutorial para compartilhar o conteúdo do seu site de forma eficaz.">
+<!-- Imagem da Publicação -->
+<meta property="og:image" content="http://lfsystemprime.com/images/logo-lfsystem.jpg">
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:width" content="800">
+<meta property="og:image:height" content="600">
+<!-- Tipo do site -->
+<meta property="og:type" content="article">
+<meta property="article:author" content="lfranca">
+<meta property="article:section" content="Tutoriais">
+<meta property="article:tag" content="Facebook, tags, og, open graph">
+<meta property="article:published_time" content="date_time">
+
 <title><?php echo $titulo;?></title>
 <link rel="shortcut icon" href="<?php echo base_url()?>public/images/logo2.ico">
 
@@ -37,7 +59,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			js = d.createElement(s); js.id = id;
 
 			js.src = "//connect.facebook.net/en_US/sdk.js";
-			
+
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 		</script>
